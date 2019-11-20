87348573e27c39ae86523ba8c3f280ba196a414f
subhankar <p.ranjan@iiitmanipur.ac.in>
2019-11-20 19:21:12 +0530

    first commit

--- rss.py
+++ rss.py
@@ -1,10 +1,10 @@
-from flask import Flask,remote_template
+from flask import Flask,render_template
 
-app=Flsk(__name__)
+app=Flask(__name__)
 @app.route("/")
 def home():
   return "hello" 
 @app.route("/rss")
 def rss():
-  return remote_template("rss.html")
+  return render_template("rss.html")
 app.run(host='0.0.0.0',port=80,debug=True)
